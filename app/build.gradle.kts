50c9fd031388987d347fe3f202ed8fcd6056bd98
Pedro Sudario <pedrohenriquesudariosilva@gmail.com>
2024-09-15 01:08:53 -0300

    Main XML + Binding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,8 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
+        dataBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
--- app/src/main/java/com/example/kt_motivation/MainActivity.kt
+++ app/src/main/java/com/example/kt_motivation/MainActivity.kt
@@ -1,47 +1,18 @@
 package com.example.kt_motivation
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.kt_motivation.ui.theme.Kt_motivationTheme
+import com.example.kt_motivation.databinding.MainBinding
 
 class MainActivity : ComponentActivity() {
+    private lateinit var binding: MainBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Kt_motivationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        binding = MainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Kt_motivationTheme {
-        Greeting("Android")
-    }
 }
