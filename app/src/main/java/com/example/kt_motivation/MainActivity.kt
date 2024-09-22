0b63704599f74d08a75b674cdc8dbe2fd1ea9213
Pedro Sudario <pedrohenriquesudariosilva@gmail.com>
2024-09-22 01:48:23 -0300

    Event click + main page

--- app/src/main/java/com/example/kt_motivation/MainActivity.kt
+++ app/src/main/java/com/example/kt_motivation/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.example.kt_motivation
-
 import android.os.Bundle
-import android.widget.Toast
+import android.view.View
 import androidx.activity.ComponentActivity
 import com.example.kt_motivation.databinding.MainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), View.OnClickListener {
     private lateinit var binding: MainBinding;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonNewPhrase.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_new_phrase) {
+            var s = ""
+        }
     }
 
 
